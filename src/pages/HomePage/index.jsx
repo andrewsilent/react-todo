a11cff4c19b03cfef735b90a12ea1339d6620de1
andrewsilent <silent.netix@gmail.com>
2021-03-09 20:20:02 +0200

    add homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import Taskboard from '../../components/Taskboard';
 import styles from './HomePage.module.scss';
 
 const HomePage = props => {
   return (
-    <main>
-      <h1 className={styles.title}>Homepage</h1>
-    </main>
+    <div className={styles.page}>
+      <header className={styles.header}>
+        <div className={styles.container}>
+          <h1 className={styles.title}>Welcome to your first taskboard</h1>
+          <h2 className={styles.subtitle}>Manage your time effectively</h2>
+        </div>
+      </header>
+      <main className={styles.content}>
+        <div className={styles.container}>
+          <Taskboard />
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <div className={styles.container}>
+          <p className={styles.copyright}>2021 © All rights reserved</p>
+        </div>
+      </footer>
+    </div>
   );
 };
 
