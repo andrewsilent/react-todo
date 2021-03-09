b2b06cc29cd0f69ea760b7227eda9b27183027d1
andrewsilent <silent.netix@gmail.com>
2021-03-09 18:40:55 +0200

    add basic structure

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './HomePage.module.scss';
+
+const HomePage = props => {
+  return (
+    <main>
+      <h1 className={styles.title}>Homepage</h1>
+    </main>
+  );
+};
+
+export default HomePage;
